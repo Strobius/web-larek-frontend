430a1fe152714570f99ad2ac6ea164eca4db4ac1
Maxim <belyaevm858@gmail.com>
2024-08-10 01:29:59 +0500

    comm2

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,33 @@
-interface Tovar  {
+export interface IProduct  {
     name: string;
+    id: string;
     description: string;
     price: number;
     imageUrl: string;
     category: string;
 };
 
-interface UserData {
-    paymentMethod: string;
-    deliveryAddress: string;
+export interface IData {
+    productCatalog: IProduct[];
+
+}
+
+export interface IContactForm {
     email: string;
     phone: string;
+}
+
+export interface IAdress {
+    address: string;
+}
+
+export interface IPage {
+    catalog: HTMLElement[];
+    locked: boolean;
+}
+
+export interface IBascket { 
+    numberPosition: number;
+    totalPrice: number;
+    selected: HTMLElement[];
 }
